98cd746e8a314fd937718373063a0501f27f140f
Vitaly Fedorov <fedvit@yandex.ru>
2018-08-04 10:55:39 +0300

    default image add

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import emptyImage from './icons/emptyImage.jpg'
 class Book extends Component {
 
     render() {
@@ -6,7 +7,7 @@ class Book extends Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail || '' }")` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : emptyImage}")` }}></div>
                     <div className="book-shelf-changer">
                         <select
                         onChange={(event) => this.props.moveShelf(
